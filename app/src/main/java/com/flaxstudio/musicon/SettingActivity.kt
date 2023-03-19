709cec48ee39586b5114b60d0750fcd1f33c3e2c
nitesh-dev <niteshdev547@gmail.com>
2023-03-20 01:10:07 +0530

    basic app structure completed

--- app/src/main/java/com/flaxstudio/musicon/SettingActivity.kt
+++ app/src/main/java/com/flaxstudio/musicon/SettingActivity.kt
@@ -1,18 +1,11 @@
 package com.flaxstudio.musicon
 
-import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // setTheme(R.style.Theme_blue)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_setting)
     }
-
 }
---
+++ app/src/main/java/com/flaxstudio/musicon/fragments/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.flaxstudio.musicon.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.flaxstudio.musicon.R
+import com.flaxstudio.musicon.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // write here...
+
+        // use below code to navigate
+        //findNavController().navigate(R.id.action_homeFragment_to_musicFragment)
+
+    }
+}
