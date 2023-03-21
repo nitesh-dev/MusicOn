b4b1cf9d0b42b054f40f88769fc54e5244ce2309
nitesh-dev <niteshdev547@gmail.com>
2023-03-21 20:29:50 +0530

    Music play design create

--- app/src/main/java/com/flaxstudio/musicon/fragments/MusicFragment.kt
+++ app/src/main/java/com/flaxstudio/musicon/fragments/MusicFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.flaxstudio.musicon.R
 import com.flaxstudio.musicon.databinding.FragmentHomeBinding
 import com.flaxstudio.musicon.databinding.FragmentMusicBinding
@@ -27,5 +28,6 @@ class MusicFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // write here...
+        //binding.playCard.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.pink_700))
     }
 }
